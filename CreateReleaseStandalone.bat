4694235897908076bbe0a13f53d2a9c364e0e92e
Luis rodas <luis.rodas@digitalstudio.com>
2018-10-06 12:05:12 -0700

    *Fix Migrate project to react 0.57.2

--- CreateReleaseStandalone.bat
+++ CreateReleaseStandalone.bat
@@ -3,6 +3,5 @@ if not exist .\android\app\src\main\assets mkdir .\android\app\src\main\assets
 call react-native bundle --dev false --platform android --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
 
 pushd android
-call gradlew.bat assembleDebug
-call gradlew.bat installDebug
+call gradlew.bat assembleRelease
 popd
