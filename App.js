f2fa45d5c95e0faab3a9d286364b9a621fda7a78
Luis rodas <luis.rodas@digitalstudio.com>
2018-10-06 02:30:43 -0700

    Revert "*Add New File format"
    This reverts commit c880db5804a7ebfc31e560febe8e10c79f1b8931.

--- App.js
+++ App.js
@@ -5,29 +5,29 @@
  * @format
  * @flow
  */
-import React, {
-    Component
-}
-from 'react';
-import {
-    Platform, StyleSheet, Text, View
-}
-from 'react-native';
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 import CMainComponentApp from './components/interface.js';
-const instructions = Platform.select(
-{
-    ios : 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu', android : 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu', 
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
 });
+
 type Props = {};
-export default class App extends Component < Props > {
-    render() {
-        return ( < View style = {
-            styles.container
-        }
-         > < CMainComponentApp  / > </View>
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <CMainComponentApp />
+      </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
