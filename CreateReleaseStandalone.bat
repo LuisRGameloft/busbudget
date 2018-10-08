7c0d2985440a8790102433ec7713a1deeafd58a8
Luis rodas <luis.rodas@digitalstudio.com>
2018-10-07 19:54:02 -0700

    +add Key release

--- CreateReleaseStandalone.bat
+++ CreateReleaseStandalone.bat
@@ -1,7 +1,5 @@
 if not exist .\android\app\src\main\assets mkdir .\android\app\src\main\assets
 
-call react-native bundle --dev false --platform android --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
-
 pushd android
 call gradlew.bat assembleRelease
 popd
